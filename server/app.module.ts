edbf28ea7843db3923c6e5224b6c5b48012a7985
devrave <devrave@protonmail.com>
2022-03-21 21:02:48 +0000

    wip: add user service.

--- server/app.module.ts
+++ server/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { ChannelModule } from "./channel/channel.module";
 import { PostModule } from "./post/post.module";
+import { User } from "./user/user.entity";
+import { UsersModule } from "./user/user.module";
 
 @Module({
-  imports: [ ChannelModule, PostModule ],
+  imports: [
+    ChannelModule,
+    PostModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "postgres",
+      port: 5432,
+      username: "test",
+      password: "test",
+      database: "test",
+      entities: [
+        User
+      ],
+      synchronize: true
+    })
+  ],
   controllers: [],
   providers: []
 })
--- server/user/user.service.ts
+++ server/user/user.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 import { nanoid } from "nanoid/async";
+import { User } from "./user.entity";
 
 const USER_PRIVATE_KEY_LENGTH = 256;
 
 @Injectable()
 export class UserService {
-  private generatePrivateKey() {
+  public constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>
+  ) {}
+
+  public async create() {
+    const user = new User();
+
+    user.privateKey = await this.generatePrivateKey();
+
+    return this.userRepository.save(user);
+  }
+
+  private async generatePrivateKey() {
     return nanoid(USER_PRIVATE_KEY_LENGTH);
   }
 }
